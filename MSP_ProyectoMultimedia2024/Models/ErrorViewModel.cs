50b2ff5b5c52c525b48976e10811b87e0d7fd596
Marcos Pantoja <marcos.sepulveda@alumnos.upla.cl>
2024-08-08 08:38:46 -0400

    Agregar archivos de proyecto.

---
+++ MSP_ProyectoMultimedia2024/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MSP_ProyectoMultimedia2024.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
