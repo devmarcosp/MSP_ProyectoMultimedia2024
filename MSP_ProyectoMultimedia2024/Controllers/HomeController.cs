646f9b096c351d73763fcc19bff3dc1695cef49d
Marcos Pantoja <marcos.sepulveda@alumnos.upla.cl>
2024-08-19 22:27:15 -0400

    Eleccion de Plantilla Bootsrap, ajustes en maqueta de index.

--- MSP_ProyectoMultimedia2024/Controllers/HomeController.cs
+++ MSP_ProyectoMultimedia2024/Controllers/HomeController.cs
@@ -23,6 +23,13 @@ namespace MSP_ProyectoMultimedia2024.Controllers
             return View();
         }
 
+        public IActionResult Productos()
+        {
+            return View();
+        }
+
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
